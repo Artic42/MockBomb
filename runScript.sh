39018b99b6ded977e178dc0eb7bc2c12954d87a4
Artic42 <main@artic42.com>
2024-10-05 10:23:45 +0200

    Run script for start up

---
+++ runScript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Navigate to the project directory
+cd /home/artic/MockBomb
+# Activate the virtual environment
+source .venv/bin/activate
+# Run the Python script
+python mockBomb.py&
