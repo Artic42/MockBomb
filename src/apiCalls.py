19829c6dceaa5044b27658803abf7191acf3a92b
Artic42 <main@artic42.com>
2024-10-05 14:38:57 +0200

    No conection exception

--- src/apiCalls.py
+++ src/apiCalls.py
@@ -2,32 +2,45 @@ import requests
 
 
 def sendAliveMessage() -> None:
-    url = "http://lsbapi.artic42.com/bomb/stillAlive"
-    payload = {}
-    headers = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return response
+    try:
+        url = "http://lsbapi.artic42.com/bomb/stillAlive"
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response
+    except Exception as e:
+        print(e)
+
 
 
 def sendDefusedMessage() -> None:
-    url = "http://lsbapi.artic42.com/bomb/disarm"
-    payload = {}
-    headers = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return response
+    try:
+        url = "http://lsbapi.artic42.com/bomb/disarm"
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response
+    except Exception as e:
+        print(e)
 
 
 def sendExplodedMessage() -> None:
-    url = "http://lsbapi.artic42.com/bomb/explode"
-    payload = {}
-    headers = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return response
+    try:
+        url = "http://lsbapi.artic42.com/bomb/explode"
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response
+    except Exception as e:
+            print(e)
 
 
 def sendArmedMessage() -> None:
-    url = "http://lsbapi.artic42.com/bomb/arm"
-    payload = {}
-    headers = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return response
+    try:
+        url = "http://lsbapi.artic42.com/bomb/arm"
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response
+    except Exception as e:
+            print(e)
